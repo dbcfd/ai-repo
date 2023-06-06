331bf292892896e5ff4ddd3f550908b858aafeef
a-7-s <aristides+a7s@aristidesstaffieri.com>
2023-06-06 09:35:22 -0600

    adds logged in button state and side panel with input for open api key, adds call to setApiKey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { Login } from "@/components"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <nav className="flex justify-end">
-        <Login />
-      </nav>
+       
     </main>
   )
 }
