06fc7c846ebd8fa24a88fb355d057d546e64c9f9
Aristides Staffieri <aristides@aristidesstaffieri.com>
2023-06-17 13:49:08 -0600

    adds apollo client and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Login } from "@/components"
 
 import { AuthProvider } from '../features/auth'
@@ -11,32 +12,41 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// What this value?
+const GQL_URL = ''
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const client = new ApolloClient({
+    uri: GQL_URL,
+    cache: new InMemoryCache()
+  });
   return (
     <html lang="en">
-      <AuthProvider>
-        <body className={inter.className}>
-          <nav className="flex justify-between items-center w-full">
-            <div className='flex items-center'>
-              <Link href='/'>
-                <h1 className='uppercase font-bold text-xl mr-24'>ai.repo</h1>
-              </Link>
-              <Link href='/finetune'>
-                <p className='uppercase text-md mr-4'>finetune</p>
-              </Link>
-              <Link href='/model'>
-                <p className='uppercase text-md mr-4'>model</p>
-              </Link>
-            </div>
-            <Login />
-          </nav>
-          {children}
-        </body>
-      </AuthProvider>
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <body className={inter.className}>
+            <nav className="flex justify-between items-center w-full">
+              <div className='flex items-center'>
+                <Link href='/'>
+                  <h1 className='uppercase font-bold text-xl mr-24'>ai.repo</h1>
+                </Link>
+                <Link href='/finetune'>
+                  <p className='uppercase text-md mr-4'>finetune</p>
+                </Link>
+                <Link href='/model'>
+                  <p className='uppercase text-md mr-4'>model</p>
+                </Link>
+              </div>
+              <Login />
+            </nav>
+            {children}
+          </body>
+        </AuthProvider>
+      </ApolloProvider>
     </html>
   )
 }
