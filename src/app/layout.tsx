02cc54cc9ec828d644b3963d3be60fa07c28a587
Aristides Staffieri <aristides@aristidesstaffieri.com>
2023-06-17 14:44:51 -0600

    fixes router not mounted invairant error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,27 +22,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ComposeDB>
-        <AuthProvider>
-          <body className={inter.className}>
-            <nav className="flex justify-between items-center w-full">
-              <div className='flex items-center'>
-                <Link href='/'>
-                  <h1 className='uppercase font-bold text-xl mr-24'>ai.repo</h1>
-                </Link>
-                <Link href='/finetune'>
-                  <p className='uppercase text-md mr-4'>finetune</p>
-                </Link>
-                <Link href='/model'>
-                  <p className='uppercase text-md mr-4'>model</p>
-                </Link>
-              </div>
-              <Login />
-            </nav>
+      <body className={inter.className}>
+        <nav className="flex justify-between items-center w-full">
+          <div className='flex items-center'>
+            <Link href='/'>
+              <h1 className='uppercase font-bold text-xl mr-24'>ai.repo</h1>
+            </Link>
+            <Link href='/finetune'>
+              <p className='uppercase text-md mr-4'>finetune</p>
+            </Link>
+            <Link href='/model'>
+              <p className='uppercase text-md mr-4'>model</p>
+            </Link>
+          </div>
+          <Login />
+        </nav>
+        <ComposeDB>
+          <AuthProvider>
             {children}
-          </body>
-        </AuthProvider>
-      </ComposeDB>
+          </AuthProvider>
+        </ComposeDB>
+      </body>
     </html>
   )
 }
